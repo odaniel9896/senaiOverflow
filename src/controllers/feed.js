63a5d5b4807eca0afd6b3075db7a5c08481ff6f3
DANIEL VITOR DE SOUZA <20185440@CFP127ALUNO.LOCAL>
2021-02-12 16:55:29 -0300

    dia 12/02, feed limit

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -23,14 +23,19 @@ module.exports = {
                     {
                         association: "Categories",
                         attributes: ["id", "description"],
+                        unique: true,
                         through: {attributes: []}
                     },
                     
                     ],
-                order: [["created_at", "DESC"]]
+                order: [["created_at", "DESC"]],
+                offset: 5, limit: 5,
+                subQuery: false
             });
 
-            res.send(feed)
+            //const hisShip = await feed.getShip()
+
+            res.send(hisShip)
         } catch (error) {
             console.log(error)
             res.status(500).send(error);
